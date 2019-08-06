ea7c98053e818bc122554a94b2cbde04c7c49695
Loic Nassif <loloynage@gmail.com>
2019-08-06 15:42:52 -0400

    added JSON compatibility using nlohmann

--- Engine/interface/config.hpp
+++ Engine/interface/config.hpp
@@ -1,7 +1,12 @@
 #pragma once
 #include "../core/headers/engine.hpp"
 #include "../renderer/headers/circle.hpp"
+#include <nlohmann/json.hpp>
 #include <utility>
+#include <fstream>
+#include <iostream>
+
+using json = nlohmann::json;
 
 namespace pe {
     void setWindowDimensions(const Engine &e, int w, int h) {
@@ -16,4 +21,29 @@ namespace pe {
     void pushShape(const Engine &e, Shape* s) {
         e.getWindowManager()->objects.push_back(s);
     }
+
+    void processJSON(const Engine &e, const std::string&& filename) {
+        std::ifstream file(filename);
+        if (file.is_open()) {
+            json data;
+            file >> data;
+
+            json objs = data["objects"];
+
+            for (json& obj : objs) {
+                std::string shape_type = obj["shape"];
+                int size = obj["size"];
+                int x_pos = obj["x_pos"];
+                int y_pos = obj["y_pos"];
+                int x_vel = obj["x_vel"];
+                int y_vel = obj["y_vel"];
+                int mass = obj["mass"];
+
+                if (shape_type == "Circle" || shape_type == "circle") {
+                    Circle *c = new Circle(Vec2<int>(x_pos, y_pos), size, Vec2<int>(x_vel, y_vel), mass);
+                    pushShape(e, c);
+                }
+            }
+        }
+    } 
 }
